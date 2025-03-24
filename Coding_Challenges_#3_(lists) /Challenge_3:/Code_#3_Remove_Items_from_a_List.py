ad5ea4c61cd6725d8fca294c81d1a9e0d9819d90
SA1010119 <1010119@pdsb.net>
2025-03-24 13:02:39 -0400

    Initial Commit

--- Coding_Challenges_#3_(lists) /Challenge_3:/Code_#3_Remove_Items_from_a_List.py
+++ Coding_Challenges_#3_(lists) /Challenge_3:/Code_#3_Remove_Items_from_a_List.py
@@ -1,17 +1,22 @@
 #-----------------------------------------------------------------------------
-# Name:
-# Purpose:
+# Name: Remove Items from a List
+# Purpose: To learn how to remove an item from a list
 
 
 #            Work on Python
 #
 # Author:      Soltan
-# Created:     -March-2025
-# Updated:     -March-2025
+# Created:     25-March-2025
+# Updated:     25-March-2025
 #----------------------------------------------------------------------------
 
-#Remove items from a list:
-
-#Creat a list
+# Create the initial to-do list
 todo_list = ['write email', 'finish homework', 'call mom', 'clean room']
-#Remove Call mom
+
+# Remove 'call mom' from the list
+todo_list.remove('call mom')
+print("After removing 'call mom':", todo_list)
+
+# Remove 'clean room' from the list
+todo_list.remove('clean room')
+print("After removing 'clean room':", todo_list)
