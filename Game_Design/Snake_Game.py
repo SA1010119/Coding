3c642d87ac15ad3ec0b5fce60f294c1307d7e4ff
SA1010119 <1010119@pdsb.net>
2025-04-23 14:26:52 -0400

    Initial Commit

---
+++ Game_Design/Snake_Game.py
@@ -0,0 +1,21 @@
+#import turtle
+#import time
+#import random
+
+delay = 0.1
+score = 0
+
+# Set up the screen
+win = turtle.Screen()
+win.title("Snake Game")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
